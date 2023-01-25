24a357c3cf83131b93d2193399f9370e2caebe24
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-01-25 07:09:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let haveWorked = 46;
+let kiloWattHour = 2.4;
+let soms =(haveWorked*kiloWattHour)
+
+console.log("Сколько нужно заплатить РЭС, 46 часов="+soms)
+
+//сколько нужно заплатить РЭС, 46 часов=110.399999
+
+let height0 = 23;
+let width0 = 33;
+let length = 50;
+let volumeWeight =length * width0 * height0 / 12; 
+console.log ("final = " + volumeWeight + "kg");
